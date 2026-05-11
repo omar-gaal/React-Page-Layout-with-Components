152cf81c62f63f17bf1ff9907ab019a4d325da12
omar-gaal <omargaal123@gmail.com>
2026-05-11 10:22:03 +0200

    delete old fetchUser code

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,8 @@ import Navigation from "./components/Navigation";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
-import { useEffect, useState } from "react";
-import MediaCard from "./components/MediaCard";
 
 export default function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    async function fetchUser() {
-      const response = await fetch(
-        "https://raw.githubusercontent.com/cederdorff/race/master/data/users.json ",
-      );
-      const data = await response.json();
-      console.log(data);
-      setUsers(data);
-    }
-    fetchUser();
-  }, []);
-
   return (
     <main className="page-layout">
       <Navigation />
